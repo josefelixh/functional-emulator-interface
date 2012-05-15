a373e5dad585f9838265501457dd04fe05070b3a
josefelixh <josefelixh@gmail.com>
2012-05-15 23:17:14 +0100

    packages orginised

--- src/main/scala/com/github/josefelixh/fei/CmdEmulator.scala
+++ src/main/scala/com/github/josefelixh/fei/CmdEmulator.scala
@@ -1,15 +1,13 @@
 package com.github.josefelixh.fei
-import com.github.josefelixh.fei.emulation.CommandLineEmulation
-import com.github.josefelixh.fei.emulation.CommandLineEmulationInput
+
+import com.github.josefelixh.fei.emulation._
 
 object CmdEmulator {
   
   def main(args: Array[String]) {
-    
     def input = new CommandLineEmulationInput(args)
-    def output = CommandLineEmulation.emulate(input)
+    def output = CommandLineEmulation emulate input
     println(output)
-    
   }
   
   
