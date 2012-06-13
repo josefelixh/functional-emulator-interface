012d6f31d82ca0fea45e4963ec975db1cc6f742b
josefelixh <josefelixh@gmail.com>
2012-06-13 23:58:46 +0100

    Hello World scala servlet

--- src/main/scala/com/github/josefelixh/fei/servlet/EmulatorServlet.scala
+++ src/main/scala/com/github/josefelixh/fei/servlet/EmulatorServlet.scala
@@ -1,13 +1,32 @@
 package com.github.josefelixh.fei.servlet
 
 import javax.servlet.http.HttpServlet
-import javax.servlet.ServletRequest
-import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import java.util.Enumeration
 
 class EmulatorServlet extends HttpServlet {
 
-	override def service(request: ServletRequest, response: ServletResponse) {
-	  response.getWriter().write("Hello scala world");
+  def write (response: HttpServletResponse, toWrite: String) : Unit = {
+    response.getWriter().write(toWrite)
+    response.getWriter().write("\r")
+  }
+  
+  def enumerate (response: HttpServletResponse, e: Enumeration[_]): Unit = {
+    while(e.hasMoreElements()) {
+      write (response, e.nextElement().toString())
+    }
+  }
+  
+	override def service(request: HttpServletRequest, response: HttpServletResponse) {
+	  write(response, "Hello scala world")
+	  
+	  enumerate(response, request.getAttributeNames())
+	  enumerate(response, request.getHeaderNames())
+	  enumerate(response, request.getParameterNames())
+	  
+	  write(response, request.getRemoteHost())
+	  write(response, request.getHeader("Host"))
 	}
-
+	
 }
